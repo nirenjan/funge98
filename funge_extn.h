3782d7b78fb6cbfb06b7ac5dd6d1e726d53aecb9
Nirenjan Krishnan <nirenjan@gmail.com>
2012-08-10 12:23:27 -0700

    Fixed extension structures to be compliant with Funge-98 spec

--- funge_extn.h
+++ funge_extn.h
@@ -27,17 +27,45 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+/****************************************************************************
+ * BrainFunge extensions - fingerprint mechanism
+ * ---------------------------------------------
+ * BrainFunge extensions are stored as follows:
+ *
+ * [Thread] -> [Funge Extension Map] -> [Extension] -> [Extension] -> ...
+ *
+ * The Funge Extension Map is an array of 26 pointers to Extension
+ * structures mapping the commands 'A' to 'Z'. At init time, the
+ * extension map has only NULL pointers, thus indicating to the
+ * extension handler that no extensions are loaded.
+ *
+ * On loading an extension, the extension map pointers are replaced
+ * with the current extension's mappings and these mappings point to
+ * the old mappings. On unloading an extension, the lists are walked
+ * and the mapping with the the correct extension ID is unloaded.
+ ***************************************************************************/
 struct funge_extn_s {
     struct funge_extn_s *next;
-    int32_t extn_id;
+    int32_t fingerprint;
     int32_t thread_id;
-    int32_t enabled_mask;
-
-    funge_function_t function[26];      
-
-    int32_t pad[2];
+    funge_extn_fn_t function;
 };
 
 typedef struct funge_extn_s funge_extn_t;
+
+struct funge_extn_map_s {
+    funge_extn_t extn_map[26];
+}
+
+typedef struct funge_extn_map_s funge_extn_map_t;
+
+/* Public API */
+int32_t funge_extn_init(void);
+
+int32_t funge_extn_handler(int32_t funge_char);
+
+int32_t funge_extn_load(int32_t fingerprint);
+
+int32_t funge_extn_unload(int32_t fingerprint);
 
 #endif /* !defined _FUNGE_EXTN_H */
